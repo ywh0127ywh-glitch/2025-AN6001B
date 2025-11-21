a4b2171bad390882978863b31435afe86a418d4f
ywh0127ywh-glitch <ywh0127ywh@gmail.com>
2025-11-21 07:36:41 +0000

    1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request
+import joblib
 
 app = Flask(__name__)
 
@@ -11,6 +12,17 @@ def main():
     q = request.form.get("q")
     print(q)
     return(render_template("main.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
+@app.route("/dbs_prediction",methods=["GET","POST"])
+def dbs_prediction():
+    q = float(request.form.get("q"))
+    model = joblib.load("dbs.pkl")
+    r = model.predict([[q]])
+    return(render_template("dbs_prediction.html",r=r[0][0]))
 
 if __name__ == "__main__":
     app.run()
